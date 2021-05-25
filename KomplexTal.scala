478ffd36b634575aa5bd2d9fe1571a3068e834b6
Polynomial Duck <felix-sjodin@hotmail.se>
2021-05-25 13:29:25 +0200

    First Update

---
+++ KomplexTal.scala
@@ -0,0 +1,28 @@
+package MandelbrotSet
+
+class Complex(var re: Double, var im: Double){
+    def +(c: Complex): Complex = { 
+        val tempRe = re 
+        val tempIm = im
+        new Complex((tempRe+c.re),(tempIm+c.im))
+    }
+    def -(c: Complex): Complex = {
+        val tempRe = re 
+        val tempIm = im 
+        new Complex ((tempRe-c.re),(tempIm-c.im))
+    }
+    def *(c: Complex): Complex = {
+        val tempRe = re 
+        val tempIm = im
+        new Complex((tempRe*c.re-tempIm*c.im),(tempRe*c.im+tempIm*c.re))
+    }
+    def /(c: Complex): Complex = {
+        val tempRe = re 
+        val tempIm = im
+        new Complex(((tempRe*c.re+tempIm*c.im)/(c.re*c.re+c.im*c.im)),((tempIm*c.re-tempRe*c.im)/(c.re*c.re+c.im*c.im)))
+    }
+    def magni: Double = (scala.math.sqrt((re*re)+(im*im)))
+    def theta = scala.math.atan2(re, im)
+    override def toString =
+        re + (if(im<0)"-"+(-im) else "+"+im) + "i"
+}
